babc71a1fa97918f5abd0e163be877b65ecd79f6
Rasmus-Lindblom2004 <143599762+Rasmus-Lindblom2004@users.noreply.github.com>
2025-11-22 16:47:15 +0100

    Viken Indoor

--- p.py
+++ p.py
@@ -19,5 +19,4 @@ for file in all_files_list:
         all_files_dict[file.split("-")[0]] = [file]
 
 for x in all_files_dict:
-    if x == 251116:
         print(f"{json.dumps(x)} : {json.dumps(all_files_dict[x])}, ")
